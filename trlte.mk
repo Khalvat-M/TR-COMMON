948f61e546514211a9f8d37e0d30ff66b371de19
khalvat <mmba313@gmail.com>
2019-04-22 09:54:12 -0400

    Build vendor.lineage.touch HAL from hardware/samsung
    this feature work in lineage and lineage based

--- trlte.mk
+++ trlte.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     init.sec.boot.sh \
     ueventd.qcom.rc
 
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.samsung
+
 # Treble
 # Include vndk/vndk-sp/ll-ndk modules
 PRODUCT_PACKAGES += \
