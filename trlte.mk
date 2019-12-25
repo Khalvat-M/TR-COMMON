364dc5081988ec41a1f93c75f5edfac8c83d12ef
Khalvat-M <mmba313@gmail.com>
2019-12-25 07:54:58 +0330

    GPS: Move gps etc config in Gnss folder

--- trlte.mk
+++ trlte.mk
@@ -190,10 +190,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-service.apq8084
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/flp.conf:/system/etc/flp.conf \
-    $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/izat.conf:/system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf
+    $(LOCAL_PATH)/gnss/gpsconfig/flp.conf:/system/etc/flp.conf \
+    $(LOCAL_PATH)/gnss/gpsconfig/gps.conf:/system/etc/gps.conf \
+    $(LOCAL_PATH)/gnss/gpsconfig/izat.conf:/system/etc/izat.conf \
+    $(LOCAL_PATH)/gnss/gpsconfig/sap.conf:/system/etc/sap.conf
 
 # IR Blaster
 PRODUCT_PACKAGES += \
