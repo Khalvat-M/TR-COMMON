a5ddfed065a3a725b27283ff3779a39dd613bd04
Khalvat-M <mmba313@gmail.com>
2019-12-25 07:51:13 +0330

    Add qcom privapp permissions

--- trlte.mk
+++ trlte.mk
@@ -222,6 +222,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Radio
 PRODUCT_PACKAGES += \
     libsecnativefeature
