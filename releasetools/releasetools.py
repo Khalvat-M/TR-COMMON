eee37a6b7470ec4cd724c28567a1ee28dd18bd57
Khalvat-M <mmba313@gmail.com>
2019-12-30 06:41:17 +0330

    Fix releasetools for Q

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2017 The LineageOS Project
+#
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,7 +20,22 @@ import common
 import re
 import os
 
-def FullOTA_InstallEnd(info):
-  info.script.Mount("/system")
-  info.script.AppendExtra('assert(run_program("/tmp/install/bin/variant_blobs.sh") == 0);')
-  info.script.Unmount("/system")
+def FullOTA_PostValidate(info):
+  info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+  info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+  info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+
+def FullOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.input_zip)
+
+def IncrementalOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.target_zip)
+
+def AddBootloaderAssertion(info, input_zip):
+  android_info = input_zip.read("OTA/android-info.txt")
+  m = re.search(r"require\s+version-bootloader\s*=\s*(\S+)", android_info)
+  if m:
+    bootloaders = m.group(1).split("|")
+    if "*" not in bootloaders:
+      info.script.AssertSomeBootloader(*bootloaders)
+    info.metadata["pre-bootloader"] = m.group(1)
